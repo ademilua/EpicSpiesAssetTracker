956488bc054c09640ff9aff4af43836010e8eba8
Tolulope <tolesy2003@outlook.com>
2018-03-06 15:49:44 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="spylogoImage" runat="server" Height="50px" Width="50px" />
+        <br />
+        <br />
+        Spy New Assignment<br />
+        <br />
+        Spy Code Name:
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        New Assignment Name:<asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        End Date of Previous Assignment:<br />
+        <asp:Calendar ID="firstCalendar" runat="server" Height="80px" Width="80px"></asp:Calendar>
+        <br />
+        Start Date of New Assignment:<br />
+        <asp:Calendar ID="secondCalendar" runat="server" Height="80px" Width="80px"></asp:Calendar>
+        <br />
+        <br />
+        Projected End Date of New Assignment:<br />
+        <asp:Calendar ID="thirdCalendar" runat="server" Height="80px" Width="80px"></asp:Calendar>
+        <br />
+        <asp:Button ID="okButton" runat="server" Text="Assign Spy" OnClick="okButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            spylogoImage.ImageUrl = "epic-spies-logo.jpg";
+          
+            if(!Page.IsPostBack)
+            {
+
+                DateTime myValue = DateTime.Now.Date;
+                firstCalendar.SelectedDate = myValue;
+                firstCalendar.VisibleDate = firstCalendar.SelectedDate;
+
+                DateTime myValue1 = firstCalendar.SelectedDate.AddDays(14);
+                secondCalendar.SelectedDate = myValue1;
+                secondCalendar.VisibleDate = secondCalendar.SelectedDate;
+
+                DateTime myValue2 = firstCalendar.SelectedDate.AddDays(21);
+                thirdCalendar.SelectedDate = myValue2;
+                thirdCalendar.VisibleDate = thirdCalendar.SelectedDate;
+
+            }
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string name = firstTextBox.Text; 
+            string Assign = secondTextBox.Text;
+            double m = 500.0;
+            string minus;
+
+            if (secondCalendar.SelectedDate > thirdCalendar.SelectedDate)
+            {
+                minus = secondCalendar.SelectedDate.Subtract(thirdCalendar.SelectedDate).TotalDays.ToString();
+            }
+            else
+            {
+                minus = thirdCalendar.SelectedDate.Subtract(secondCalendar.SelectedDate).TotalDays.ToString();
+            }
+            double j = double.Parse(minus);
+            double p = m * j;
+            if (j > 7)
+            {
+                p += 1000.0; 
+            }
+
+           
+            string result = string.Format("Assignment {0} , {1}, is Authorized and the total buget is : {2:C}", name, Assign, p);
+            resultLabel.Text = result;
+
+            TimeSpan timeBetweenAssignments = secondCalendar.SelectedDate.Subtract(firstCalendar.SelectedDate);
+            
+            if(timeBetweenAssignments.TotalDays < 14 || timeBetweenAssignments.TotalDays > 14)
+            {
+                resultLabel.Text = "Error: Must allow at least two weeks between" +   "previous assignment and new assignment.";
+                DateTime myValue1 = firstCalendar.SelectedDate.AddDays(14);
+                secondCalendar.SelectedDate = myValue1;
+                secondCalendar.VisibleDate = secondCalendar.SelectedDate;
+            }
+            } 
+    }
+}
